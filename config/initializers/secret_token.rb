81fc3566dd01bb57051a8d97f0b9164d31187472
Richard <rbarrera87@gmail.com>
2013-11-19 19:39:15 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PruebaZipCodes::Application.config.secret_token = '308984df2571e2123ff064436cea1e5a1bcb5f9513a1b905d310ea62026aff139d4f472faefe19ae7cc755b04d67835377ce44416986d88ad48dc51f60c2c6da'
